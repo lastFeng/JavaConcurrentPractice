3eb1a29ffe3d6260328d9e3dc6b3b0411b7abca9
lastFeng <563805728@qq.com>
2021-05-13 15:43:18 +0800

    chapter 5

---
+++ src/chapter5/FileCrawler.java
@@ -0,0 +1,90 @@
+package chapter5;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FileCrawler implements Runnable{
+    private final BlockingQueue<File> fileQueue;
+    private final FileFilter fileFilter;
+    private final File root;
+
+    public FileCrawler(BlockingQueue<File> fileQueue, FileFilter fileFilter, File root) {
+        this.fileQueue = fileQueue;
+        this.fileFilter = fileFilter;
+        this.root = root;
+    }
+
+    @Override
+    public void run() {
+        try {
+            crawl(root);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void crawl(File root) throws InterruptedException {
+        File[] files = root.listFiles(fileFilter);
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    crawl(file);
+                } else if (!alreadyIndexed(file)) {
+                    fileQueue.put(file);
+                }
+            }
+        }
+    }
+
+    private boolean alreadyIndexed(File file) {
+        String canonicalPath = file.getAbsolutePath() + file.getName();
+        // 这里检查数据库是否已经有了这个文件
+        return false;
+    }
+
+    public static void startIndexing(File[] roots) {
+        BlockingQueue<File> queue = new LinkedBlockingQueue<>(265);
+        FileFilter fileFilter = pathname -> true;
+
+        for (File root : roots) {
+            new Thread(new FileCrawler(queue, fileFilter, root)).start();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            new Thread(new Indexer(queue)).start();
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File("D:\\Workspaces\\work\\JavaConcurrentPractice\\src");
+        startIndexing(new File[]{file});
+    }
+}
+
+class Indexer implements Runnable{
+    private final BlockingQueue<File> queue;
+
+    public Indexer(BlockingQueue<File> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                indexFile(queue.take());
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void indexFile(File take) {
+        System.out.println(take.getAbsolutePath() +take.getName());
+        System.out.println("do index file...");
+        System.out.println("---------");
+    }
+}
+
---
+++ src/chapter5/SemaphoreTest.java
@@ -0,0 +1,28 @@
+package chapter5;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+    public void timeTask(int nThread, final Runnable task) throws InterruptedException {
+        final Semaphore semaphore = new Semaphore(nThread / 2);
+
+        for (int i = 0; i < nThread; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    task.run();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                } finally {
+                    semaphore.release();
+                }
+            });
+            t.start();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SemaphoreTest semaphoreTest = new SemaphoreTest();
+        semaphoreTest.timeTask(4, () -> System.out.println("task: " + Thread.currentThread().getName()));
+    }
+}
