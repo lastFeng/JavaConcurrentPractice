152fbe540218e0fbd2c49cbf401f4a40acd91491
lastFeng <563805728@qq.com>
2021-05-25 11:11:01 +0800

    chapter13

---
+++ src/chapter13/ReadWriteMap.java
@@ -0,0 +1,72 @@
+package chapter13;
+
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteMap<K, V> {
+    private final Map<K, V> map;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock r = lock.readLock();
+    private final Lock w = lock.writeLock();
+
+    public ReadWriteMap(Map<K, V> map) {
+        this.map = map;
+    }
+
+    public V put(K key, V value) {
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public V remove(K key) {
+        w.lock();
+        try {
+            return map.remove(key);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public boolean remove(K key, V value) {
+        w.lock();
+        try {
+            return map.remove(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public void putAll(Map<? extends K, ? extends V> m) {
+        w.lock();
+        try {
+            map.putAll(m);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public void clear() {
+        w.lock();
+
+        try {
+            map.clear();
+        } finally {
+            w.unlock();
+        }
+    }
+
+    public V get(K key) {
+        r.lock();
+        try {
+            return map.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+}
