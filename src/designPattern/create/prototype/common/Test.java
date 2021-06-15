f40f80b68f95f52a156a6a963487ea55f2fe296d
lastFeng <563805728@qq.com>
2021-06-15 18:02:01 +0800

    原型模式

---
+++ src/designPattern/create/prototype/common/Test.java
@@ -0,0 +1,10 @@
+package designPattern.create.prototype.common;
+
+public class Test {
+    public static void main(String[] args) {
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+}
